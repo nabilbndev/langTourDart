c1c41db97d74020e4827eb3376b3df6873bc1279
Nabil <mahmud.stu20192@juniv.edu>
2023-02-24 19:22:05 +0600

    added a function that prints number

--- basic.dart
+++ basic.dart
@@ -2,7 +2,13 @@ void printString(String frameworkName) {
   print('I think $frameworkName is good.');
 }
 
+void printNum(int number) {
+  print('$number gets printed');
+}
+
 void main() {
   String gglOwnedFW = "Flutter";
+  int evenNumber = 20;
   printString(gglOwnedFW);
+  printNum(evenNumber);
 }
