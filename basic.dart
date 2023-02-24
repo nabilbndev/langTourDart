cee80f1bdf0762c82339cdcb95f4282551afe8c5
Nabil <mahmud.stu20192@juniv.edu>
2023-02-24 19:31:16 +0600

    added a function which adds 2 numbers

--- basic.dart
+++ basic.dart
@@ -7,10 +7,16 @@ void printNum(int number) {
   print('$number gets printed');
 }
 
+int addsNumber(int x, int y) {
+  return x + y;
+}
+
 //The function down below is the main function.
 void main() {
   String gglOwnedFW = "Flutter";
   int evenNumber = 20;
   printString(gglOwnedFW);
   printNum(evenNumber);
+  addsNumber(5, 6);
+  print(addsNumber(7, 8));
 }
