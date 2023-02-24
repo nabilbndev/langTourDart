e50655f77a690c1578ba81a15aa41d9861a2bdbe
Nabil <mahmud.stu20192@juniv.edu>
2023-02-24 19:24:59 +0600

    added two lines of comments

--- basic.dart
+++ basic.dart
@@ -1,3 +1,4 @@
+//Following are functions which get called from the main() function.
 void printString(String frameworkName) {
   print('I think $frameworkName is good.');
 }
@@ -6,6 +7,7 @@ void printNum(int number) {
   print('$number gets printed');
 }
 
+//The function down below is the main function.
 void main() {
   String gglOwnedFW = "Flutter";
   int evenNumber = 20;
