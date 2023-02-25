64db6dedeb1f55b16bd8fe83abbdeb61cfecea1c
Md Nabil Mahmud <75440292+nabilbndev@users.noreply.github.com>
2023-02-25 09:30:53 +0600

    corrected spelling mistake

--- basic.dart
+++ basic.dart
@@ -28,6 +28,6 @@ String printCountry(String country) {
   return country;
 }
 //added this line of comment from the sub-branch in github repo
-/*This is a comment from the locally-craeted branch
+/*This is a comment from the locally-created branch
 This branch will be pushed to github
 */
